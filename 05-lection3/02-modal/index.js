68929a2b320a4bbecd0401c693de1028d627991c
Lika <87615829+Lika1100@users.noreply.github.com>
2024-03-18 20:22:41 +0300

    modal

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,7 +2,7 @@
     const close = document.getElementById('modal-close');
     const modal = document.getElementById('modal');
     const modalOpen = document.getElementById('modal-open');
-
+    
     if (!close || !modalOpen || !modal) {
         return;
     }
